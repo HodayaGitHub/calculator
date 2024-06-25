074eb0e1bf1746daaeb20b70eb587c2f9778991d
HodayaGitHub <hodaya1812@gmail.com>
2024-06-25 12:58:42 +0300

    Add a test scenario - no token is provided

--- tests/token.spec.ts
+++ tests/token.spec.ts
@@ -1,10 +1,11 @@
 import request from 'supertest';
 import http from 'http';
 import express, { Express, Request, Response, NextFunction } from 'express';
-import { verifyToken } from '../middlewares/auth.middleware'; 
-import { calculateRoutes } from '../api/calculate/calculate.routes'; 
+import { verifyToken } from '../middlewares/auth.middleware';
+import { calculateRoutes } from '../api/calculate/calculate.routes';
 import { generateToken } from '../utils/jwt.utils';
-
+import { AUTH_HEADER } from '../common/consts';
+import jwt from 'jsonwebtoken';
 
 process.env.JWT_SECRET = 'testsecret';
 
@@ -13,15 +14,23 @@ const server: http.Server = http.createServer(app);
 let token: string;
 
 beforeAll(() => {
-  token = generateToken(); 
+    token = generateToken();
 });
 
 app.use('/api/calculate', calculateRoutes);
 
 app.get('/test-verify-token', verifyToken, (req, res) => {
-  res.json({ message: 'Token verified successfully' });
+    res.json({ message: 'Token verified successfully' });
 });
 
 
 describe('Auth Middleware Tests', () => {
+    it('should return 400 if token format is invalid', (done) => {
+        request(server)
+          .get('/test-verify-token')
+          .set('Authorization', 'InvalidTokenFormat')
+          .expect(400)
+          .end(done);
+      });
+    
 });
